4bfd2100eae542e66977453623e6aeb350f77acd
Minsu Kim <m.kim006064@gmaiil.com>
2023-09-23 22:49:18 +1200

    added portfolio buttons, added fade in animation

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,15 +9,28 @@ function Work() {
       <h2 className="section__title section__title--work">Personal Projects</h2>
       <p className="section__subtitle section__subtitle--work">A selection of my range of work</p>
       <div className="portfolio">
-        <a href="#" className="portfolio__item">
+        <div className="portfolio__item">
           <img src={snapvote} alt="" className="portfolio__img" />
-        </a>
-        <a href="#" className="portfolio__item">
-          <img src={portfolio} alt="" className="portfolio__img" />
-        </a>
-        <a href="#" className="portfolio__item">
+          <div className="portfolio__btn-container">
+            <a href="https://snapvote-11223.web.app/" className="portfolio__btn">Website</a>
+            <a href="https://github.com/Minsukim2827/SnapVote" className="portfolio__btn">Github</a>
+          </div>
+        </div>
+        <div className="portfolio__item">
           <img src={spotifyDiscordBot} alt="" className="portfolio__img" />
-        </a>
+          <div className="portfolio__btn-container">
+            <a href="https://github.com/Minsukim2827/DiscordSpotifyBot" className="portfolio__btn">Github</a>
+           
+          </div>
+        </div>
+        <div className="portfolio__item">
+          <img src={portfolio} alt="" className="portfolio__img" />
+          <div className="portfolio__btn-container">
+            <a href="https://main.d27iti8adrqv7z.amplifyapp.com/" className="portfolio__btn">Website</a>
+            <a href="https://github.com/Minsukim2827/portfolio-react-app" className="portfolio__btn">Github</a>
+          </div>
+        </div>
+
       </div>
     </section>
   );
